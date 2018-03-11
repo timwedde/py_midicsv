7a2ae6c6f3c113141dfabcd010550cbb72d93203
Tim Wedde <timwedde@icloud.com>
2018-03-11 14:07:53 +0100

    Port of core methods

---
+++ pydicsv/csvmidi.py
@@ -0,0 +1,39 @@
+def out_byte(byte):
+    pass
+
+def out_event(byte):
+    pass
+
+def out_meta(byte):
+    pass
+
+def out_short(short):
+    pass
+
+def out_bytes(byte, len):
+    pass
+
+def out_varlen(num):
+    pass
+
+def x_fields(n, fbias):
+    pass
+
+def n_fields(n):
+    pass
+
+def check_bytes(field_number, len):
+    pass
+
+def get_csv_line(file):
+    pass
+
+def clamp(value, min, max, field):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    # TODO: Set up argparse
+    main()
---
+++ pydicsv/midicsv.py
@@ -0,0 +1,15 @@
+def vlength(track, len):
+    pass
+
+def text_to_csv(file, text, len):
+    pass
+
+def track_to_csv(file, track_number, track, trac_length, ppq):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    # TODO: Set up argparse
+    main()
